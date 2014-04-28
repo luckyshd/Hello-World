83ffac94c77e7da1dd94b94a3a17c64b89328849
Forrest Shi <shihuadesysu@gmail.com>
2014-04-28 17:46:13 +0800

    Update SetTest.sh

--- SetTest.sh
+++ SetTest.sh
@@ -1,14 +1,13 @@
-et=$1
+#!/bin/bash
+
+
+
+Set=$1
 SetResultFile=$2
 
 StartTime=`date`
-sleep 60
-
-echo ""
-
-
+sleep 5
 EndTime=`date`
 
-echo "StartTime is       ${StartTime}">>${SetResultFile}
-echo "EndTime   is       ${EndTime}">>${SetResultFile}
-
+echo "StartTime is    ${StartTime}">>${SetResultFile}
+echo "EndTime   is    ${EndTime}">>${SetResultFile}
